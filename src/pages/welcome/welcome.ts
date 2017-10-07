abd55ea044a02d72e91c97be2e303c46f1e8fe47
mbj36 <mohitbajo36@gmail.com>
2017-10-07 15:53:19 +0530

    Changes done

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
-import { GooglePlus } from '@ionic-native/google-plus';
-import firebase from 'firebase'; 
+
 /**
  * The Welcome Page is a splash page that quickly describes the app,
  * and then directs the user to create an account or log in.
@@ -13,36 +12,15 @@ import firebase from 'firebase';
   selector: 'page-welcome',
   templateUrl: 'welcome.html'
 })
-
 export class WelcomePage {
-  userProfile: any = null;
-  constructor(public navCtrl: NavController, private googlePlus: GooglePlus) { 
-    firebase.auth().onAuthStateChanged(user =>{
-      if(user){
-        this.userProfile = user;
-      } else {
-        this.userProfile = null;
-      }
-    })
+
+  constructor(public navCtrl: NavController) { }
+
+  login() {
+    this.navCtrl.push('LoginPage');
   }
 
-  loginUser(): void {
-    this.googlePlus.login({
-      'webClientId':'223296465079-df1kr53o1dkir3bvpopbium7jca1pcrk.apps.googleusercontent.com',
-      'offline': true
-    }).then( res => {
-            const googleCredential = firebase.auth.GoogleAuthProvider
-                .credential(res.idToken);
-   
-            firebase.auth().signInWithCredential(googleCredential)
-          .then( response => {
-              console.log("Firebase success: " + JSON.stringify(response));
-          });
-    }, err => {
-        console.error("Error: ", err)
-    });
+  signup() {
+    this.navCtrl.push('SignupPage');
   }
-
 }
-
-
