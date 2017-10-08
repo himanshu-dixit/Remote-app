9d52f9f2fd4e33c9f53622effb6aea001baca48e
mbj36 <mohitbajo36@gmail.com>
2017-10-08 14:38:12 +0530

    changes

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -46,7 +46,7 @@ export class TutorialPage {
   }
 
   startApp() {
-    this.navCtrl.setRoot('WelcomePage', {}, {
+    this.navCtrl.setRoot('LoginPage', {}, {
       animate: true,
       direction: 'forward'
     });
